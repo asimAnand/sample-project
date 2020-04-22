d0dcda25a2ca57425761e4d98f477e164beb1135
asimAnand <asimanand.anand@gmail.com>
2020-04-22 08:15:03 +0530

    Sorting each connected component

--- solution.cpp
+++ solution.cpp
@@ -4,13 +4,13 @@
 
 using namespace open3d;
 
-//Utility function to do a Depth First Search to get the connected components
+// Utility function to do a Depth First Search to get the connected components
 void geometry::TriangleMesh::DFSUtil(int vertex, bool visited[], list<int>& connected_component){
-    //Mark the current vertex as visited and add to the list
+    // Mark the current vertex as visited and add to the list
     visited[vertex] = true;
     connected_component.push_back(vertex);
     
-    //Loop for all vertices adjacent to this vertex having same color
+    // Loop for all vertices adjacent to this vertex having same color
     list<int>::iterator i;
     for (i = adjacency_list_[vertex].begin(); i != adjacency_list_[vertex].end(); ++i)
         if (!visited[*i] && vertex_colors_[vertex] == vertex_colors_[*i])
@@ -18,31 +18,29 @@ void geometry::TriangleMesh::DFSUtil(int vertex, bool visited[], list<int>& conn
 }
 
 list<list<int> > geometry::TriangleMesh::IdenticallyColoredConnectedComponents(){
-    list<list<int> > connected_components_list;   //To store the list of connected components
-    list<int> connected_component                 //To store one connected component
+    list<list<int> > connected_components_list;   // To store the list of connected components
+    list<int> connected_component                 // To store one connected component
     int v;
 
-    //Compute adjacency list for the given mesh
+    // Compute adjacency list for the given mesh
     ComputeAdjacencyList();
     
-    //Mark all the vertices as not visited
+    // Mark all the vertices as not visited
     bool *visited = new bool[vertices_.size()];
     for (v = 0; v < vertices_.size(); v++)
 	visited[v] = false;
 
     for (v = 0; v < vertices_.size(); v++){
         if (visited[v] == false){
-            //All reachable vertices from v having the same color
+            // All reachable vertices from v having the same color
             DFSUtil(v, visited, connected_component);
-	    connected_components_list.push_back(connected_component); 
+            // Add sorted connected component to the list of connected components
+	    connected_components_list.push_back(connected_component.sort()); 
         }
         connected_component.clear();
     }
     
-    /* Sort the list by smallest number in each list. Here each connected component in the
-     * list of connected components is already sorted ascendingly by the vertex because of
-     * the way we are traversing the adjacency list.
-     */ 
+    // Sort the list by smallest number in each connected component     
     connected_components_list.sort();
 
     delete[] visited;
@@ -63,16 +61,16 @@ void pybind_trianglemesh(py::module &m){
 int main(){
     geometry::TriangleMesh mesh;
 
-    //Read triangle mesh "test_mesh.ply"
+    // Read triangle mesh "test_mesh.ply"
     io::ReadTriangleMesh("test_mesh.ply", *mesh, true);
     
     auto connected_components = mesh.IdenticallyColoredConnectedComponents();
 
-    //Print connected_components in the specified format
+    // Print connected_components in the specified format
     ofstream outfile;
     outfile.open("results.txt");
     
-    //Loop over the nested list connected_components
+    // Loop over the nested list connected_components
     list<list<int> >::iterator outer_list;
     list<int>::iterator inner_list;
     for (outer_list = connected_components.begin(); outer_list != connected_components.end(); outer_list++){
