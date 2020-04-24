60b06c742c5774f5159e247d9ec47a64e016f461
asimAnand <asimanand.anand@gmail.com>
2020-04-24 13:50:25 +0530

    Changes to allow an executable to be built

--- solution.cpp
+++ solution.cpp
@@ -1,28 +1,31 @@
 #include "Open3D/Open3D.h"
 #include <fstream>
-#include <pybind11/pybind11.h>
+#include <vector>
+//#include <pybind11/pybind11.h>
 
 using namespace open3d;
 
+std::vector<int> connected_component;
+
 // Utility function to do a Depth First Search to get the connected components
-void geometry::TriangleMesh::DFSUtil(int vertex, bool visited[], list<int>& connected_component){
-    // Mark the current vertex as visited and add to the list
+void geometry::TriangleMesh::DFSUtil(int vertex, bool visited[], std::vector<int> connected_component){
+    // Mark the current vertex as visited and add to the vector
     visited[vertex] = true;
     connected_component.push_back(vertex);
     
     // Loop for all vertices adjacent to this vertex having same color
-    list<int>::iterator i;
+    std::unordered_set<int>::iterator i;
     for (i = adjacency_list_[vertex].begin(); i != adjacency_list_[vertex].end(); ++i)
         if (!visited[*i] && vertex_colors_[vertex] == vertex_colors_[*i])
 	    DFSUtil(*i, visited, connected_component);     
 }
 
-list<list<int> > geometry::TriangleMesh::IdenticallyColoredConnectedComponents(){
-    list<list<int> > connected_components_list;   // To store the list of connected components
-    list<int> connected_component                 // To store one connected component
+std::vector<std::vector<int> > geometry::TriangleMesh::IdenticallyColoredConnectedComponents(){
+    std::vector<std::vector<int> > connected_components_vector;   // To store the vector of connected components
+    std::vector<int> connected_component;                 // To store one connected component
     int v;
 
-    // Compute adjacency list for the given mesh
+    // Compute adjacency vector for the given mesh
     ComputeAdjacencyList();
     
     // Mark all the vertices as not visited
@@ -34,48 +37,39 @@ list<list<int> > geometry::TriangleMesh::IdenticallyColoredConnectedComponents()
         if (visited[v] == false){
             // All reachable vertices from v having the same color
             DFSUtil(v, visited, connected_component);
-            // Add sorted connected component to the list of connected components
-	    connected_components_list.push_back(connected_component.sort()); 
+            // Add sorted connected component to the vector of connected components
+	    std::sort(connected_component.begin(), connected_component.end()); 
+            connected_components_vector.push_back(connected_component);
             connected_component.clear();
         }
     }
     
-    // Sort the list by smallest number in each connected component     
-    connected_components_list.sort();
+    // Sort the vector by smallest number in each connected component     
+    std::sort(connected_components_vector.begin(), connected_components_vector.end());
 
     delete[] visited;
 
-    return connected_components_list;
+    return connected_components_vector;
 }
 
-/*
- * This is the same pybind_trianglemesh present in 
- * Open3D/src/Python/open3d_pybind/geometry/trianglemesh.cpp
- */
-void pybind_trianglemesh(py::module &m){
-    m.def("identically_colored_connected_components", 
-          &geometry::TriangleMesh::IdenticallyColoredConnectedComponents,
-          "Function to compute identically-colored connected components in a triangle mesh");
-}
-	      
 int main(){
     geometry::TriangleMesh mesh;
 
     // Read triangle mesh "test_mesh.ply"
-    io::ReadTriangleMesh("test_mesh.ply", *mesh, true);
+    io::ReadTriangleMesh("test_mesh.ply", mesh, true);
     
     auto connected_components = mesh.IdenticallyColoredConnectedComponents();
 
     // Print connected_components in the specified format
-    ofstream outfile;
+    std::ofstream outfile;
     outfile.open("results.txt");
     
-    // Loop over the nested list connected_components
-    list<list<int> >::iterator outer_list;
-    list<int>::iterator inner_list;
-    for (outer_list = connected_components.begin(); outer_list != connected_components.end(); outer_list++){
-        for (inner_list = (*outer_list).begin(); inner_list != (*outer_list).end(); inner_list++){
-	    outfile << *inner_list << " ";
+    // Loop over the nested vector connected_components
+    std::vector<std::vector<int> >::iterator outer_vector;
+    std::vector<int>::iterator inner_vector;
+    for (outer_vector = connected_components.begin(); outer_vector != connected_components.end(); outer_vector++){
+        for (inner_vector = (*outer_vector).begin(); inner_vector != (*outer_vector).end(); inner_vector++){
+	    outfile << *inner_vector << " ";
         }
         outfile << std::endl;
     }
